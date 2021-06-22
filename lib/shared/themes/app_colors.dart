ec16e1995933e4e4645c4411423bf35562e7af73
Germano Ricardi <germanoricardi7@gmail.com>
2021-06-21 23:34:27 -0400

    Commit inicial do projeto PayFlow desenvolvido em Flutter

---
+++ lib/shared/themes/app_colors.dart
@@ -0,0 +1,12 @@
+import  'package:flutter/material.dart';
+
+class AppColors{
+  static final primary    = Color(0xFFFF941A); // Comuns
+  static final gray       = Color(0xFF585666); // Comuns
+  static final delete     = Color(0xFFE83F5B); // Comuns
+  static final heading    = Color(0xFF585666); // Textos
+  static final body       = Color(0xFF706E7A); // Textos
+  static final stroke     = Color(0xFFE3E3E6); // Items
+  static final shape      = Color(0xFFFAFAFC); // Items
+  static final background = Color(0xFFFFFFFF); // Items
+}
---
+++ lib/shared/themes/app_text_styles.dart
@@ -0,0 +1,111 @@
+import  'package:flutter/material.dart';
+import  'package:google_fonts/google_fonts.dart';
+import  'package:payflow/shared/themes/app_colors.dart';
+
+class TextStyles {
+  static final titleHome = GoogleFonts.lexendDeca(
+    color:      AppColors.heading,
+    fontSize:   36,
+    fontWeight: FontWeight.w600,
+  );
+  static final titleRegular = GoogleFonts.lexendDeca(
+    color:      AppColors.heading,
+    fontSize:   20,
+    fontWeight: FontWeight.w400,
+  );
+  static final titleBoldHeading = GoogleFonts.lexendDeca(
+    color:      AppColors.heading,
+    fontSize:   20,
+    fontWeight: FontWeight.w600,
+  );
+  static final titleBoldBackground = GoogleFonts.lexendDeca(
+    color:      AppColors.background,
+    fontSize:   20,
+    fontWeight: FontWeight.w600,
+  );
+  static final titleListTile = GoogleFonts.lexendDeca(
+    color:      AppColors.heading,
+    fontSize:   17,
+    fontWeight: FontWeight.w600,
+  );
+  static final trailingRegular = GoogleFonts.lexendDeca(
+    color:      AppColors.heading,
+    fontSize:   16,
+    fontWeight: FontWeight.w400,
+  );
+  static final trailingBold = GoogleFonts.lexendDeca(
+    color:      AppColors.heading,
+    fontSize:   16,
+    fontWeight: FontWeight.w600,
+  );
+  static final buttonPrimary = GoogleFonts.inter(
+    color:      AppColors.primary,
+    fontSize:   17,
+    fontWeight: FontWeight.w400,
+  );
+  static final buttonHeading = GoogleFonts.inter(
+    color:      AppColors.heading,
+    fontSize:   17,
+    fontWeight: FontWeight.w400,
+  );
+  static final buttonGray = GoogleFonts.inter(
+    color:      AppColors.gray,
+    fontSize:   17,
+    fontWeight: FontWeight.w400,
+  );
+  static final buttonBackground = GoogleFonts.inter(
+    color:      AppColors.background,
+    fontSize:   17,
+    fontWeight: FontWeight.w400,
+  );
+  static final buttonBoldPrimary = GoogleFonts.inter(
+    color:      AppColors.primary,
+    fontSize:   17,
+    fontWeight: FontWeight.w700,
+  );
+  static final buttonBoldHeading = GoogleFonts.inter(
+    color:      AppColors.heading,
+    fontSize:   17,
+    fontWeight: FontWeight.w700,
+  );
+  static final buttonBoldGray = GoogleFonts.inter(
+    color:      AppColors.gray,
+    fontSize:   17,
+    fontWeight: FontWeight.w700,
+  );
+  static final buttonBoldBackground = GoogleFonts.inter(
+    color:      AppColors.background,
+    fontSize:   17,
+    fontWeight: FontWeight.w700,
+  );
+  static final captionBackground = GoogleFonts.lexendDeca(
+    color:      AppColors.background,
+    fontSize:   13,
+    fontWeight: FontWeight.w400,
+  );
+  static final captionShape = GoogleFonts.lexendDeca(
+    color:      AppColors.shape,
+    fontSize:   13,
+    fontWeight: FontWeight.w400,
+  );
+  static final captionBody = GoogleFonts.lexendDeca(
+    color:      AppColors.body,
+    fontSize:   13,
+    fontWeight: FontWeight.w400,
+  );  
+  static final captionBoldBackground = GoogleFonts.lexendDeca(
+    color:      AppColors.background,
+    fontSize:   13,
+    fontWeight: FontWeight.w600,
+  );
+  static final captionBoldShape = GoogleFonts.lexendDeca(
+    color:      AppColors.shape,
+    fontSize:   13,
+    fontWeight: FontWeight.w600,
+  );  
+  static final captionBoldBody = GoogleFonts.lexendDeca(
+    color:      AppColors.body,
+    fontSize:   13,
+    fontWeight: FontWeight.w600,
+  );
+}
